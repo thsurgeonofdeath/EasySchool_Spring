6c373028ac8e27c225ea534d5eef37f8478cb9da
thsurgeonofdeath <salahek25@gmail.com>
2023-02-06 18:42:47 +0100

    init

---
+++ src/test/java/com/salaheddine/schoolboot/SchoolbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salaheddine.schoolboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
