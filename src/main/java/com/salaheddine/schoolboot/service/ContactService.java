eccbbefef84d272a294552b512777936dedbc3b5
thsurgeonofdeath <salahek25@gmail.com>
2023-02-08 11:28:10 +0100

    scopes

--- src/main/java/com/salaheddine/schoolboot/service/ContactService.java
+++ src/main/java/com/salaheddine/schoolboot/service/ContactService.java
@@ -4,10 +4,24 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import com.salaheddine.schoolboot.model.Contact;
+import org.springframework.web.context.annotation.ApplicationScope;
 
 @Slf4j
 @Service
+@ApplicationScope
 public class ContactService {
+    private int counter = 0;
+    public ContactService() {
+        System.out.println("Contact Bean initialized");
+    }
+    public int getCounter() {
+        return counter;
+    }
+
+    public void setCounter(int counter) {
+        this.counter = counter;
+    }
+
     public boolean saveMessageDetails(Contact contact) {
         boolean isSaved = true;
         //TODO - Need to persist the data into the DB table
