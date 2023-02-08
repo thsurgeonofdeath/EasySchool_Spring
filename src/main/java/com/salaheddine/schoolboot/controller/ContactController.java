35bcc7d57863774582c62edacdda5c7721578c70
thsurgeonofdeath <salahek25@gmail.com>
2023-02-08 10:47:58 +0100

    bean validator applied to contact form

--- src/main/java/com/salaheddine/schoolboot/controller/ContactController.java
+++ src/main/java/com/salaheddine/schoolboot/controller/ContactController.java
@@ -2,15 +2,22 @@ package com.salaheddine.schoolboot.controller;
 
 import com.salaheddine.schoolboot.model.Contact;
 import com.salaheddine.schoolboot.service.ContactService;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+
 import java.util.logging.Logger;
 
+@Slf4j
 @Controller
 public class ContactController {
     private final ContactService contactService;
@@ -19,13 +26,18 @@ public class ContactController {
         this.contactService = contactService;
     }
     @RequestMapping("/contact")
-    public String displayContactPage(){
+    public String displayContactPage(Model model){
+        model.addAttribute("contact", new Contact());
         return "contact";
     }
     @RequestMapping(value = "/saveMsg", method = RequestMethod.POST)
-    public ModelAndView saveMessage(Contact contact){
+    public String saveMessage(@Valid @ModelAttribute("contact") Contact contact, Errors errors){
+        if(errors.hasErrors()){
+            log.error("Contact form validation failed due to : " + errors.toString());
+            return "contact.html";
+        }
         contactService.saveMessageDetails(contact);
-        return new ModelAndView("redirect:/contact");
+        return "redirect:/contact";
     }
 
 }
