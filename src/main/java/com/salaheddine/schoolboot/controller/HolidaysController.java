b7314cd78336550c209f08c2f8e06464deb9d39e
thsurgeonofdeath <salahek25@gmail.com>
2023-02-08 10:12:12 +0100

    playing with params

--- src/main/java/com/salaheddine/schoolboot/controller/HolidaysController.java
+++ src/main/java/com/salaheddine/schoolboot/controller/HolidaysController.java
@@ -4,6 +4,8 @@ import com.salaheddine.schoolboot.model.Holiday;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Arrays;
 import java.util.List;
@@ -11,6 +13,7 @@ import java.util.stream.Collectors;
 
 @Controller
 public class HolidaysController {
+
     @GetMapping("/holidays")
     public String displayHolidays(Model model) {
         List<Holiday> holidays = Arrays.asList(
--- src/main/java/com/salaheddine/schoolboot/service/ContactService.java
+++ src/main/java/com/salaheddine/schoolboot/service/ContactService.java
@@ -1,14 +1,13 @@
 package com.salaheddine.schoolboot.service;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import com.salaheddine.schoolboot.model.Contact;
 
+@Slf4j
 @Service
 public class ContactService {
-    private static final Logger log = LoggerFactory.getLogger(ContactService.class);
     public boolean saveMessageDetails(Contact contact) {
         boolean isSaved = true;
         //TODO - Need to persist the data into the DB table
