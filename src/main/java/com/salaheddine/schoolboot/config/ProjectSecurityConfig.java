8c784206779b849564b553bc2e43694312e9ecb1
thsurgeonofdeath <salahek25@gmail.com>
2023-02-09 11:15:11 +0100

    holidays table

--- src/main/java/com/salaheddine/schoolboot/config/ProjectSecurityConfig.java
+++ src/main/java/com/salaheddine/schoolboot/config/ProjectSecurityConfig.java
@@ -21,6 +21,7 @@ public class ProjectSecurityConfig {
                 .requestMatchers("/closeMsg/**").hasRole("ADMIN")
                 .requestMatchers("/home").permitAll()
                 .requestMatchers("/holidays/**").permitAll()
+                .requestMatchers("/holidays").permitAll()
                 .requestMatchers("/contact").permitAll()
                 .requestMatchers("/saveMsg").permitAll()
                 .requestMatchers("/courses").permitAll()
--- src/main/java/com/salaheddine/schoolboot/controller/HolidaysController.java
+++ src/main/java/com/salaheddine/schoolboot/controller/HolidaysController.java
@@ -1,6 +1,8 @@
 package com.salaheddine.schoolboot.controller;
 
 import com.salaheddine.schoolboot.model.Holiday;
+import com.salaheddine.schoolboot.repository.HolidaysRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,19 +15,12 @@ import java.util.stream.Collectors;
 
 @Controller
 public class HolidaysController {
+    @Autowired
+    private HolidaysRepository holidaysRepository;
 
     @GetMapping("/holidays")
     public String displayHolidays(Model model) {
-        List<Holiday> holidays = Arrays.asList(
-                new Holiday(" Jan 1 ","New Year's Day", Holiday.Type.FESTIVAL),
-                new Holiday(" Oct 31 ","Halloween", Holiday.Type.FESTIVAL),
-                new Holiday(" Nov 24 ","Thanksgiving Day", Holiday.Type.FESTIVAL),
-                new Holiday(" Dec 25 ","Christmas", Holiday.Type.FESTIVAL),
-                new Holiday(" Jan 17 ","Martin Luther King Jr. Day", Holiday.Type.FEDERAL),
-                new Holiday(" July 4 ","Independence Day", Holiday.Type.FEDERAL),
-                new Holiday(" Sep 5 ","Labor Day", Holiday.Type.FEDERAL),
-                new Holiday(" Nov 11 ","Veterans Day", Holiday.Type.FEDERAL)
-        );
+        List<Holiday> holidays = holidaysRepository.findAllHolidays();
         Holiday.Type[] types = Holiday.Type.values();
         for (Holiday.Type type : types) {
             model.addAttribute(type.toString(),
@@ -34,3 +29,5 @@ public class HolidaysController {
         return "holidays.html";
     }
 }
+
+
